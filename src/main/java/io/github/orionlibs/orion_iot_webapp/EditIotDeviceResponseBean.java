8c358153ad368c86c676c69b0d00ab93119ebb77
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2024-06-24 18:35:16 +0100

    controller to edit IoT device details

---
+++ src/main/java/io/github/orionlibs/orion_iot_webapp/EditIotDeviceResponseBean.java
@@ -0,0 +1,15 @@
+package io.github.orionlibs.orion_iot_webapp;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@NoArgsConstructor
+//@AllArgsConstructor
+@SuperBuilder
+@Getter
+@Setter
+public class EditIotDeviceResponseBean extends OrionResponse
+{
+}
---
+++ src/main/java/io/github/orionlibs/orion_iot_webapp/page/EditIoTDeviceController.java
@@ -0,0 +1,33 @@
+package io.github.orionlibs.orion_iot_webapp.page;
+
+import io.github.orionlibs.orion_iot.device_details.DeviceDAO;
+import io.github.orionlibs.orion_iot.device_details.DeviceModel;
+import io.github.orionlibs.orion_iot_webapp.EditIotDeviceRequestBean;
+import io.github.orionlibs.orion_iot_webapp.EditIotDeviceResponseBean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/v1/iot-devices")
+public class EditIoTDeviceController
+{
+    @PutMapping(consumes = "application/json")
+    public ResponseEntity<EditIotDeviceResponseBean> addIoTDevicePageAddDevice(@RequestBody EditIotDeviceRequestBean requestBean)
+    {
+        try
+        {
+            DeviceModel device = DeviceDAO.getByID(requestBean.getDeviceID());
+            device.setDeviceName(requestBean.getDeviceName());
+            device.setConnectionURL(requestBean.getConnectionURL());
+            DeviceDAO.update(device);
+            return ResponseEntity.ok(EditIotDeviceResponseBean.builder().build());
+        }
+        catch(Throwable e)
+        {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+}
